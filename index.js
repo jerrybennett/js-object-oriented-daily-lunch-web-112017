ca36e6ab7e147c899d4142d31348c81532e08335
mattfaircloth <mattcfaircloth@gmail.com>
2018-01-11 16:04:49 -0500

    working to associate all classes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let store ={ customers: [], employers: [] };
-let customerId = 0;
+let store = {customers: [], employers: [], meals: [], deliveries: []}
 
+let customerId = 0;
 class Customer {
 
   constructor(name, employer) {
@@ -28,8 +28,8 @@ class Customer {
   totalSpent() {
 
   }
-
 }
+
 let employerId = 0;
 class Employer {
   constructor(name) {
@@ -40,23 +40,83 @@ class Employer {
 
 
   employees() {
-    
+    return store.customers.filter(customer => {
+      return customer.employerId === this.id
+    })
   }
 
   deliveries() {
-    
+    return store.deliveries.filter(delivery => {
+      return delivery.employerId === this.id
+    })
   }
 
   meals() {
-    
+    return store.meals.filter(meal => {
+      return meal.employerId === this.id
+    })
   }
 
   mealTotals() {
-    
+
   }
-  
 }
 
-class Meal() {
-  
+let mealId = 0;
+class Meal {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
+    store.meals.push(this);
+  }
+
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id
+    })
+  }
+
+  customers() {
+    return store.customers.filter(customer => {
+      return customer.mealId === this.id
+    })
+  }
+}
+
+let deliveryId = 0;
+class Delivery {
+  constructor(meal, customer) {
+    this.meal = meal;
+    this.customer = customer;
+    this.id = ++deliveryId;
+    store.deliveries.push(this);
+    if(meal) {
+      this.mealId = meal.id;
+    }
+
+    if(customer) {
+      this.customerId = customer.id;
+    }
+  }
+
+  setMeal() {
+    this.mealId = meal.id
+  }
+
+  setCustomer() {
+    this.customerId = customer.id;
+  }
+
+  meal() {
+    return store.meals.find(function(meal){
+      return meal.id === this.mealId
+    })
+  }
+
+  customer() {
+    return store.customers.find(function(customer){
+      return customer.id === this.customerId
+    })
+  }
 }
